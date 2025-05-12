bf315886bab11ef96139166684ad103115ff279a
Huvison <huvisoncollins@gmail.com>
2025-05-12 11:52:25 +0000

    feat: add Task model

---
+++ src/main/java/org/example/taskmanagementsystem/models/Task.java
@@ -0,0 +1,49 @@
+package org.example.taskmanagementsystem.models;
+
+import java.sql.Date;
+
+public class Task {
+    private int id;
+    private int userId;
+    private String title;
+    private String description;
+    private String status;
+    private Date dueDate;
+
+    public Task(int userId,String title, String description, String status, Date dueDate) {
+        this.userId = userId;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    //getters
+    public int getId() {
+        return id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public Date getDueDate() {
+        return dueDate;
+    }
+    public int getUserId() {
+        return userId;
+    }
+
+    //setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+}
